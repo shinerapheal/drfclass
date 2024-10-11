312e0de216f6ae2521defbcd2e5a1d45ed0ac8ca
shinerapheal <raphealshine@gmail.com>
2024-10-11 11:42:28 +0530

    defenetion added

--- myenv/p2/app2/views.py
+++ myenv/p2/app2/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from . serializers import *
 
 # Create your views here.
+#class view
 class EmpViewSet(viewsets.ModelViewSet):
     serializer_class=EmpSeralizer
     queryset=Emp.objects.all()
